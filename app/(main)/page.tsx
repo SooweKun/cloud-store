cb8987e599f73a5aae0f302edfc55a40f0dfe92e
SooweKun <apextoplega@mail.ru>
2026-01-28 05:46:01 +0300

    feat: add recents file and home module

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,3 +1,9 @@
+import { HomeModule } from '@/src/modules/home/module';
+
 export default function Home() {
-  return <div className='w-full h-screen flex justify-center items-center text-amber-50'></div>;
+  return (
+    <div className='w-full flex justify-center items-center flex-1'>
+      <HomeModule />
+    </div>
+  );
 }
---
+++ src/modules/home/module.tsx
@@ -0,0 +1,65 @@
+import Test2 from '@/src/assets/file-item-test2.jpg';
+import Test from '@/src/assets/file-items-test.png';
+import mdIco from '@/src/assets/ico/md-ico.svg';
+import { FileItem } from '../files/feature/file-item';
+
+const Arr = [
+  {
+    created_at: '22.01.2026',
+    data: [
+      {
+        image: Test2,
+        name: 'file name',
+        expansion: 'png',
+      },
+      {
+        image: Test,
+        name: 'file name',
+        expansion: 'png',
+      },
+      {
+        image: mdIco,
+        name: 'file name',
+        expansion: 'md',
+      },
+    ],
+  },
+];
+
+export const HomeModule = () => {
+  return (
+    <div className='w-full flex gap-[30px] justify-center items-center'>
+      <div className='flex flex-col gap-[20px]'>
+        <div className='flex gap-[30px]'>
+          <div className='w-[350px] h-[170px] bg-[#272727] rounded-[10px]' />
+          <div className='w-[350px] h-[170px] bg-[#272727] rounded-[10px]' />
+          <div className='w-[350px] h-[170px] bg-[#272727] rounded-[10px]' />
+        </div>
+        <div className='w-[1110px] h-[480px] bg-[#272727] rounded-[10px] p-[15px] pb-0 flex flex-col gap-[25px]'>
+          <div className='w-full h-[30px] flex justify-between'>
+            <div className='flex flex-col gap-0'>
+              <h1 className='text-[20px]'>Recents Files</h1>
+              <p className='text-[12px] pl-[12px]'>last 2 days</p>
+            </div>
+          </div>
+          <div className='flex gap-[20px] pl-[15px] overflow-y-auto'>
+            {Arr.map(({ created_at, data }) => (
+              <div className='flex flex-col gap-[5px]' key={created_at}>
+                <p className='text-[14px]'>{created_at}</p>
+                <div className='flex gap-[20px] flex-wrap'>
+                  {data.map(({ image, name, expansion }) => (
+                    <FileItem image={image} name={name} expansion={expansion} key={name} created_at={created_at} />
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className='flex flex-col gap-[20px]'>
+        <div className='w-[350px] h-[430px] bg-[#272727] rounded-[10px]' />
+        <div className='w-[350px] h-[220px] bg-[#272727] rounded-[10px]' />
+      </div>
+    </div>
+  );
+};
